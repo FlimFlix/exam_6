0910a3390a361d0c4e64b4a76b83770be427bbfc
Archer <arseniy.tarapon@gmail.com>
2019-01-12 13:42:16 +0600

    сделал этап 1

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import UserInfo, Post
 
-# Register your models here.
+
+class UserInfoInline(admin.StackedInline):
+    model = UserInfo
+    filter_horizontal = ('friend', )
+
+
+class UserInfoAdmin(UserAdmin):
+    inlines = [UserInfoInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserInfoAdmin)
+# admin.site.register(UserInfo)
+admin.site.register(Post)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='user', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+    friend = models.ManyToManyField(User, blank=True, related_name='friend', verbose_name='friend')
+    photo = models.ImageField(blank=True, null=True, verbose_name='Аватар')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='article', on_delete=models.PROTECT, verbose_name='Пользователь')
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(max_length=3000, verbose_name='Текст')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return "%s. %s (%s)" % (self.pk, self.title, self.date_created)
